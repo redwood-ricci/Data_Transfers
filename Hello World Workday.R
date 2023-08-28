e4790f98b49d83de94f534b1e189cc85683c1c5f
mricci <michael.ricci@redwood.com>
2023-08-28 12:15:05 -0400

    Initial commit of workday API download

---
+++ Hello World Workday.R
@@ -0,0 +1,33 @@
+source("ActiveCo Functions.R")
+library(odbc)
+library(httr)
+# Endpoint, Credentials, and Other Variables
+# AP endpoint
+# endpoint_url <- "https://services1.myworkday.com/ccx/service/customreport2/redwood/bhanuj.pant@redwood.com/Analytics_-__AP_Suppliers__Details?IRS_1099_Supplier=0&format=csv"
+
+endpoint_url <- "https://services1.myworkday.com/ccx/service/customreport2/redwood/bhanuj.pant@redwood.com/Analytics_-__AP_Suppliers__Details?IRS_1099_Supplier=0&format=csv"
+# xlsx.url <- "https://services1.myworkday.com/ccx/service/customreport2/redwood/bhanuj.pant%40redwood.com/Analytics_-__AP_Suppliers__Details?IRS_1099_Supplier=0&format=csv"
+username <- "Report_ISU_TestUser"
+password <- "1wVYA8rY1Y57nE@W"
+
+# API Request
+response <- GET(url = endpoint_url, authenticate(username, password, type = "basic"))
+
+# Check if request was successful and content type is CSV
+if (http_type(response) == "text/csv" && status_code(response) == 200) {
+  csv_content <- content(response, "text")
+  df <- read.csv(textConnection(csv_content), header = TRUE)
+#   head(df)
+} else {
+  stop("Failed to retrieve CSV data from Workday!")
+}
+
+ssms.con <- dbConnect(odbc(),
+                      Driver = "SQL Server",
+                      Server = "WIN-6PDIPKC0EER",
+                      Database = "Analytics_Source",
+                      UID = SSMS_USR,
+                      PWD = SSMS_PASS)
+
+dbWriteTable(ssms.con,'R_Analytics_AP_Supplier_Details',df,overwrite = TRUE)
+
