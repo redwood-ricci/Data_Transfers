d0ecabd8977ca15b18738544854e691789faea76
Kasee Renukachari <renukachari.kasee@redwood.com>
2023-08-29 08:03:43 -0400

    there are some changes are made on this code. we have added the timestap to the data load

--- Hello World Workday.R
+++ Hello World Workday.R
@@ -5,6 +5,9 @@ library(httr)
 # AP endpoint
 # endpoint_url <- "https://services1.myworkday.com/ccx/service/customreport2/redwood/bhanuj.pant@redwood.com/Analytics_-__AP_Suppliers__Details?IRS_1099_Supplier=0&format=csv"
 
+
+est_now <- with_tz(Sys.time(), "America/New_York")
+
 endpoint_url <- "https://services1.myworkday.com/ccx/service/customreport2/redwood/bhanuj.pant@redwood.com/Analytics_-__AP_Suppliers__Details?IRS_1099_Supplier=0&format=csv"
 # xlsx.url <- "https://services1.myworkday.com/ccx/service/customreport2/redwood/bhanuj.pant%40redwood.com/Analytics_-__AP_Suppliers__Details?IRS_1099_Supplier=0&format=csv"
 username <- "Report_ISU_TestUser"
@@ -17,10 +20,13 @@ response <- GET(url = endpoint_url, authenticate(username, password, type = "bas
 if (http_type(response) == "text/csv" && status_code(response) == 200) {
   csv_content <- content(response, "text")
   df <- read.csv(textConnection(csv_content), header = TRUE)
+  
 #   head(df)
 } else {
   stop("Failed to retrieve CSV data from Workday!")
 }
+
+df$now <- est_now
 
 ssms.con <- dbConnect(odbc(),
                       Driver = "SQL Server",
@@ -29,5 +35,7 @@ ssms.con <- dbConnect(odbc(),
                       UID = SSMS_USR,
                       PWD = SSMS_PASS)
 
-dbWriteTable(ssms.con,'R_Analytics_AP_Supplier_Details',df,overwrite = TRUE)
+dbWriteTable(ssms.con,'R_Analytics_AP_Supplier_Details',df, append = TRUE) #overwrite = TRUE)
+
+
 
