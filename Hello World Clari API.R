cf2af024a3b50358d56600648afef5e920e72123
redwood-ricci <113071501+redwood-ricci@users.noreply.github.com>
2024-06-21 15:15:02 -0400

    Can now request and return forecast from Clari

--- Hello World Clari API.R
+++ Hello World Clari API.R
@@ -8,101 +8,74 @@ export.url <- "https://api.clari.com/v4/export/forecast/"
 api_key <- "f7ad3871-2233-4b85-b81e-8b1209551f66474998-0"
 forecastId <- "forecast_new_summary"
 # Set your API key or token for authentication
-
-# Add headers for authentication and content type. This might vary based on the API's requirements.
-# headers <- c(
-#   'apikey' = paste(api_key),
-#   #   'Content-Type' = 'application/json',
-#   'forecastId' = forecastId
-# )
-# 
-# # Make the GET request (adjust parameters and method as needed)
-# response <- POST(
-#   paste0(export.url,forecastId),
-#   add_headers(.headers=headers)
-# )
-# 
-# content(response,'text', encoding = "UTF-8")
-send.headers <- 
-  headers <- c(
-    'apikey' = paste(api_key),
-    'forecastId' = forecastId,
-    "includeHistorical"= 'true',
-    "timePeriod" = "2024_Q2",
-    "scopeId" = "332801::MGR",
-    "exportFormat"= "CSV"
-  )
-make.forecast <- POST(
-  paste0("https://api.clari.com/v4/export/forecast/",forecastId),
-  add_headers(.headers = send.headers)
-)
-jobid <- fromJSON(content(make.forecast,'text',encoding = 'UTF-8'))
-# jobid$jobId
-response.headers <- c(
-  'apikey' = api_key
-)
-
-forecast.response <- GET(
-  paste0("https://api.clari.com/v4/ingest/job/",jobid$jobId,"/results"),
-  add_headers(.headers = response.headers)
-)
-# zzz <- content(forecast.response)
-# x <- zzz$entries
-# fcst.rpt <- fromJSON(content(forecast.response))
-
-
 headers <- c(
-  'apikey' = paste(api_key),
-   'Content-Type' = 'application/json',
-    forecastId = 'forecast_new_summary',
-    typesToExport = 'forecast_updated',
-    scopeId = '1905::MGR'
-)
-# Define the request body
-# body <- list(
-
-# )
-
-# Convert body to JSON if needed
-# body_json <- toJSON(body)
-
-# Make the GET request with a body
-response <- GET(
-  paste0(url,forecastId),
-  add_headers(.headers=headers)
+  'apikey' = paste(api_key)
 )
 
-  # 'scopeId' = '{type:FORECAST ROLE , userId:332801, userType:MGR}'
-  # 'scopeId' = "{\\\"type\\\":\\\"FORECAST_ROLE\\\",\\\"userId\\\":332801,\\\"userType\\\":\\\"MGR\\\"}"
-  # 'scopeId' = "{\"type\":\"FORECAST_ROLE\",\"userId\":332801,\"userType\":\"MGR\"}"
-  # 'scopeId' = "{type:FORECAST_ROLE,userId:332801,userType:MGR}"
-  #'timePeriod' = '2024_Q1'
+# list of report parameters
+query.params <- list(
+  includeHistorical = TRUE,
+  # timePeriod = "2024_Q2", # clari defaults to current quarter
+  typesToExport = c('forecast','quota','forecast_updated','adjustment','crm_total','crm_closed')
+  )
+# convert params to JSON for API call
+query.json <- toJSON(query.params,auto_unbox = TRUE)
 
 # Make the GET request (adjust parameters and method as needed)
-# response <- GET(
-#   paste0(url,forecastId),
-#   add_headers(.headers=headers),verbose()
-#   )
+response <- POST(
+  paste0(export.url,forecastId),
+  add_headers(.headers=headers),
+  body = query.json
+)
+  # read the forecast export Job ID
+jobid <- fromJSON(content(response,'text',encoding = 'UTF-8'))
 
-# print(response$request$headers)
-
-# Check the status code of the response
-if (status_code(response) == 200) {
-  # Parse the response body from JSON format
-  data <- content(response, "parsed")
-  print(data)
-} else {
-  print(paste("Error in API request. Status code:", status_code(response)))
+# send a request for the status of the export job
+minutes.to.wait <- 5
+print("Pausing for 10 seconds for report to process")
+Sys.sleep(10) # pause for 10 seconds
+for( i in 1:minutes.to.wait){
+  status <- GET(paste0("https://api.clari.com/v4/export/jobs/",jobid[[1]]),
+                add_headers(.headers=headers)
+  )
+  status.response <- fromJSON(content(status,'text',encoding = 'UTF-8'))
+  
+  if(status.response$job$status != "DONE"){
+    print("Report not ready, waiting another minute")
+    Sys.sleep(60) # pause for one minute if the job is not done
+  }else{
+    print("Forecast Export Ready for Pickup! Sending GET Request")
+    break
+  } # else break the waiting loop
 }
-content(response,'text', encoding = "UTF-8")
+# get the content of the forecast export
+forecast.report.response <- GET(paste0("https://api.clari.com/v4/export/jobs/",jobid[[1]],"/results"),
+                                add_headers(.headers=headers)
+)
 
-f.cast <- fromJSON(content(response,'text', encoding = "UTF-8"),flatten = TRUE)
+f.cast <- fromJSON(content(forecast.report.response,'text',encoding = 'UTF-8'),flatten = TRUE)
 
-deals <- f.cast$entries
-reps <- f.cast$users
+entries <- f.cast$entries
+users <- f.cast$users
 field.map <- f.cast$fields
-time.map <- f.cast$timePeriods
-timeframe.map <- f.cast$timeFrames
+time.period.map <- f.cast$timePeriods
+time.frame.map <- f.cast$timeFrames
+
+# scrub column names for BigQuery
+names(entries) <- clean.names(names(entries))
+names(users) <- clean.names(names(users))
+names(field.map) <- clean.names(names(field.map))
+names(time.period.map) <- clean.names(names(time.period.map))
+names(time.frame.map) <- clean.names(names(time.frame.map))
+
+
+
+upload.to.bigquery(entries,"Clari","Entries")
+upload.to.bigquery(users,"Clari","Users")
+upload.to.bigquery(field.map,"Clari","Field_Map")
+upload.to.bigquery(time.period.map,"Clari","Time_Period_Map")
+upload.to.bigquery(time.frame.map,"Clari","Time_Frame_Map")
+
 
 
 
